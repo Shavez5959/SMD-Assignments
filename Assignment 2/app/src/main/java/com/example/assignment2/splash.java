e92285099d2921c09adaf46520f716ee6f12bc46
Shavez777 <l230777@lhr.nu.edu.pk>
2026-03-15 22:00:55 +0500

    Added splash and onboarding page

---
+++ Assignment 2/app/src/main/java/com/example/assignment2/splash.java
@@ -0,0 +1,63 @@
+package com.example.assignment2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splash extends AppCompatActivity {
+
+    ImageView logo,name;
+    Animation fade_transition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.splash), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        init();
+
+        setAnimations();
+
+        movetoOnboarding();
+
+    }
+
+    private void movetoOnboarding()
+    {
+        new Handler().postDelayed(()->{
+            startActivity(new Intent(splash.this, onboarding.class));
+        },5000);
+
+    }
+
+
+    void setAnimations()
+    {
+        logo.setAnimation(fade_transition);
+        name.setAnimation(fade_transition);
+    }
+
+    private void init()
+    {
+        logo=findViewById(R.id.logo);
+        name=findViewById(R.id.name);
+        fade_transition= AnimationUtils.loadAnimation(this, R.anim.fade_transition);
+    }
+}
+
+
